a2b932b08677acf6b817e70f94a9d31a79f13a4d
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-10-28 17:04:39 +0300

    a

--- Controllers/User.js
+++ Controllers/User.js
@@ -173,6 +173,5 @@ exports.handleTwitterAuth = asyncHandler(async (req, res) => {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
-  // res.redirect(`${process.env.CLIENT_URL}/`);
-  return res.status(200).json({ message: "User logged in successfully" });
+  return res.redirect(`http://localhost:3000/`);
 });
