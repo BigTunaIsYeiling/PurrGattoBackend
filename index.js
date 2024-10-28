4c915e1aca2ce5a138388dd9d3d75e6152240814
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-10-28 14:00:59 +0300

    iu

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const connectDB = require("./Configs/Database");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
+const cors = require("cors");
 require("dotenv").config();
 require("./Configs/passport");
 
@@ -12,7 +13,6 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 
 app.use(
   session({
@@ -23,6 +23,13 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+    credentials: true,
+  })
+);
 
 app.use("/user", require("./Routes/userRoutes"));
 
