168e9d7bce9e2cd49a64d8f660683bddda80fa10
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-01 17:44:20 +0200

    as

--- Controllers/Post.js
+++ Controllers/Post.js
@@ -32,14 +32,19 @@ exports.createReplyPost = async (req, res) => {
   try {
     const id = req.user;
     const { PostBody, messageId, parentPostId } = req.body;
+
     if (PostBody.length < 1)
       return res.status(400).json({ error: "Post body cannot be empty" });
+
+    // Verify that the user is authorized to create a post for the given message
     const message = await Message.findById(messageId);
     if (message.receiver.toString() !== id.toString()) {
       return res.status(400).json({
         error: "You are not authorized to create a post for this message",
       });
     }
+
+    // Create and save the new reply post
     const newPost = new Post({
       PostBody,
       messageId,
@@ -47,8 +52,20 @@ exports.createReplyPost = async (req, res) => {
       parentPost: parentPostId,
     });
     const savedPost = await newPost.save();
+
+    // Mark the message as answered
     message.isAnswered = true;
     await message.save();
+
+    // Find the top-level post and update its `updatedAt` field
+    let topLevelPost = await Post.findById(parentPostId);
+    while (topLevelPost.parentPost) {
+      topLevelPost = await Post.findById(topLevelPost.parentPost);
+    }
+    // Update `updatedAt` for the main post
+    topLevelPost.updatedAt = new Date();
+    await topLevelPost.save();
+
     res.status(201).json(savedPost);
   } catch (err) {
     res.status(400).json({ error: "Unable to create post" });
@@ -74,36 +91,53 @@ exports.likePost = async (req, res) => {
     res.status(400).json({ error: "Unable to like post" });
   }
 };
+// Function to get all replies in a flat structure for a given post
+const getAllReplies = async (postId) => {
+  const repliesQueue = [postId];
+  const allReplies = [];
+
+  while (repliesQueue.length > 0) {
+    const currentPostId = repliesQueue.shift();
+    const replies = await Post.find({ parentPost: currentPostId }).sort({
+      createdAt: -1,
+    });
+
+    // Map each reply to retrieve message and other details
+    for (const reply of replies) {
+      const submessage = await Message.findById(reply.messageId);
+      allReplies.push({
+        answer: reply.PostBody,
+        ask: submessage.content,
+        postId: reply._id,
+        createdAt: reply.createdAt,
+        likes: reply.likes,
+        message: submessage._id,
+      });
+
+      // Add reply ID to queue to check for further sub-replies
+      repliesQueue.push(reply._id);
+    }
+  }
+
+  return allReplies;
+};
 
 exports.getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
+    const AllAnswers = await Post.find({ Author: userId }).countDocuments();
+    // Fetch all top-level posts
     const posts = await Post.find({
       $and: [{ Author: userId }, { parentPost: null }],
-    }).sort({
-      createdAt: -1,
-    });
+    }).sort({ updatedAt: -1 });
 
+    // Map each post and fetch its flat list of replies
     const PostsData = await Promise.all(
       posts.map(async (post) => {
         const message = await Message.findById(post.messageId);
-        const Replies = await Post.find({ parentPost: post._id }).sort({
-          createdAt: -1,
-        });
 
-        const RepliesData = await Promise.all(
-          Replies.map(async (reply) => {
-            const submessage = await Message.findById(reply.messageId);
-            return {
-              answer: reply.PostBody,
-              ask: submessage.content,
-              postId: reply._id,
-              createdAt: reply.createdAt,
-              likes: reply.likes,
-              message: message._id,
-            };
-          })
-        );
+        // Use getAllReplies to fetch a flat list of all replies to this post
+        const RepliesData = await getAllReplies(post._id);
 
         return {
           answer: post.PostBody,
@@ -112,7 +146,8 @@ exports.getUserPosts = async (req, res) => {
           createdAt: post.createdAt,
           likes: post.likes,
           message: message._id,
-          replies: RepliesData,
+          replies: RepliesData, // All replies at the same level
+          AllAnswers: AllAnswers,
         };
       })
     );
