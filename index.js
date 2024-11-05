eee5cae3f0461dc0e286aac563c3d782b8f007d8
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-05 07:14:55 +0200

    lkopp

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Configure express-session with cross-origin cookie settings
 app.use(
   session({
