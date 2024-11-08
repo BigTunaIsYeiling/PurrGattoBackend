1c76715721c1761bf8cf1a36738c82c68c501b18
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-08 11:17:36 +0200

    lop

--- Controllers/User.js
+++ Controllers/User.js
@@ -48,8 +48,6 @@ exports.login = asyncHandler(async (req, res) => {
 
 exports.logout = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
-    httpOnly: true,
-    expires: new Date(0),
   });
   res.clearCookie("token");
   res.status(200).json({ message: "User logged out successfully" });
